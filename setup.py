925e262d66c4fa2ecc9056122caa1b461bf7d261
Sonny Zinn <sonny.zinn@noaa.gov>
2019-07-11 08:50:14 -0400

    Fix setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     author="Sonny Zinn",
     author_email="sonny.zinn@noaa.gov",
     packages=["hysplitdata", "hysplitdata.traj", "hysplitdata.conc"],
-    python_requires="=3.7",
-    install_requires=["numpy=1.16", "pytz=2019.1"]
+    python_requires="==3.7",
+    install_requires=['numpy==1.16', "pytz==2019.1"]
 )
